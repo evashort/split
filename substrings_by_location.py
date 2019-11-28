e3fb5d87698af0a1baa35a5854f7f92e3a96ac24
Eva Short <evashort73@gmail.com>
2019-11-28 14:33:21 -0800

    search for substrings in an interval

---
+++ substrings_by_location.py
@@ -0,0 +1,34 @@
+import bisect
+
+class SubstringsByLocation:
+    def __init__(self, substrings):
+        '''
+        substrings: a list of (starts, length) pairs
+        '''
+        self.substrings = substrings
+        self.startIDs = sorted(
+            (start, i) \
+                for i, (starts, _) in enumerate(self.substrings) \
+                    for start in starts
+        )
+
+    def idsInInterval(self, start, stop):
+        startIndex = bisect.bisect_left(self.startIDs, (start,))
+        stopIndex = bisect.bisect_left(self.startIDs, (stop,))
+        return (
+            (i, start) \
+                for start, i in self.startIDs[startIndex:stopIndex] \
+                    if self.substrings[i][1] <= stopIndex - i
+        )
+
+if __name__ == '__main__':
+    finder = SubstringsByLocation([
+        ([-2], 2) # 0: fully outside left
+        ([0], 2) # 1: outside left
+        ([1], 2) # 2: spans exactly
+        ([2], 2) # 3: outside right
+        ([4], 7) # 4: fully outside right
+        ([-2, 0, 1, 2, 4], 2) # 5: all of the above
+        ([0], 4) #6: outside both
+    ])
+    print(finder.idsInInterval(1, 3)) # [(2, 1), (5, 1)]
