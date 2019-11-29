20931ab693fc5e36b5f254b611ef983f0360e711
Eva Short <evashort73@gmail.com>
2019-11-29 13:04:53 -0800

    show common substrings in original order

--- main.py
+++ main.py
@@ -5,26 +5,38 @@ from pathlib import Path
 from preprocessor import preprocess
 import re
 
-def showCounts(testCase):
+def showBreakdown(testCase):
     with open(Path('testcases') / testCase) as f:
         text = f.read()
 
     tokens = preprocess(text)
     tree = makeTreeWithStopSets(tokens)
     sliceSets = getMatchingSlices(tree)
-    for starts, length in sorted(
-        sliceSets,
-        key=lambda item: len(item[0]) * item[1],
-        reverse=True
-    ):
-        start = starts[0]
-        print('{} █{}█'.format(
-            len(starts),
+    allSlices = sorted(
+        (start, -length, i, len(starts)) \
+            for i, (starts, length) in enumerate(sliceSets) \
+                for start in starts
+    )
+    for start, length, i in allSlices:
+        length = -length
+        print('{}{} {}'.format(
+            ' ' * getIndentLevel(count),
+            i,
             ''.join(tokens[start : start + length])
         ))
+
+def getIndentLevel(count):
+    if count >= 12:
+        return 0
+    if count >= 5:
+        return 4
+    elif count >= 3:
+        return 8
+    else:
+        return 12
 
 if __name__ == '__main__':
     with open('counts.txt', 'w', encoding='utf-8') as f:
         with redirect_stdout(f):
             # https://www.sitepoint.com/colors-json-example/
-            showCounts('colors.json')
+            showBreakdown('colors.json')
