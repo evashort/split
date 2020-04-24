8f4a5bf32f2c444bc1387e5e41ddb434537cb6a6
Eva Short <evashort73@gmail.com>
2020-04-24 13:34:09 -0700

    use reward instead of punishment

--- dtw.py
+++ dtw.py
@@ -17,38 +17,31 @@ for testCase in [
         text = f.read(5000)
 
     tokens = np.frombuffer(text, dtype=np.byte)
-    costs = np.full(
-        (len(tokens), len(tokens)),
-        float('inf')
-    )
-    costs[:, 0] = 0
-    offsetMultipliers = np.exp(0.003 * np.arange(len(tokens)))
+    scores = np.zeros((len(tokens), len(tokens)))
     for y in range(1, len(tokens)):
         for x in range(y):
-            costs[y, x + 1] = min(
-                costs[y, x],
-                costs[y - 1, x + 1],
-                costs[y - 1, x]
+            scores[y, x + 1] = max(
+                scores[y, x],
+                scores[y - 1, x + 1],
+                scores[y - 1, x]
             ) + (
-                tokens[x] != tokens[y]
-            ) / (
-                x - y + len(tokens)
-            ) * offsetMultipliers[y - x]
+                tokens[x] == tokens[y]
+            )
 
     result = np.zeros((len(tokens), len(tokens)), dtype=np.uint8)
 
     y = len(tokens) - 1
-    x = np.argmin(costs[-1, 1:])
+    x = np.argmax(scores[-1, 1:])
     while x > 0:
         result[y, x] = 255
-        if costs[y, x] <= costs[y - 1, x + 1]:
-            if costs[y - 1, x] <= costs[y, x]:
+        if scores[y, x] >= scores[y - 1, x + 1]:
+            if scores[y - 1, x] >= scores[y, x]:
                 x -= 1
                 y -= 1
             else:
                 x -= 1
         else:
-            if costs[y - 1, x] <= costs[y - 1, x + 1]:
+            if scores[y - 1, x] >= scores[y - 1, x + 1]:
                 x -= 1
                 y -= 1
             else:
