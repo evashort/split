e55dbd2e475d383989c1d21729a90076fc472c6a
Eva Short <evashort73@gmail.com>
2020-04-26 16:55:44 -0700

    take advantage of sparsity

--- dtw.py
+++ dtw.py
@@ -1,3 +1,4 @@
+import bisect
 import numpy as np
 from imageio import imwrite
 from pathlib import Path
@@ -17,35 +18,97 @@ for testCase in [
         text = f.read(5000)
 
     tokens = np.frombuffer(text, dtype=np.byte)
-    scores = np.zeros((len(tokens), len(tokens)))
-    for y in range(1, len(tokens)):
-        for x in range(y):
-            scores[y, x + 1] = max(
-                scores[y, x],
-                scores[y - 1, x + 1],
-                scores[y - 1, x]
-            ) + (
-                tokens[x] == tokens[y]
-            )
+    tokenPositions = {}
+    for position in reversed(range(len(tokens))):
+        tokenPositions.setdefault(tokens[position], []).append(position)
 
-    result = np.zeros((len(tokens), len(tokens)), dtype=np.uint8)
+    usedYCounts = [0 for _ in tokens]
 
-    y = len(tokens) - 1
-    x = np.argmax(scores[-1, 1:])
-    while x > 0:
-        result[y, x] = 255
-        if scores[y, x] >= scores[y - 1, x + 1]:
-            if scores[y - 1, x] >= scores[y, x]:
-                x -= 1
-                y -= 1
-            else:
-                x -= 1
-        else:
-            if scores[y - 1, x] >= scores[y - 1, x + 1]:
-                x -= 1
-                y -= 1
-            else:
-                y -= 1
+    rank = 1
+    planes = []
+    for token in tokens:
+        outerPositions = tokenPositions[token]
+        outerX = outerPositions.pop()
+        for outerYIndex in range(usedYCounts[outerX], len(outerPositions)):
+            outerY = outerPositions[-1 - outerYIndex]
+            x = outerX
+            y = outerY
+            planes.extend([] for i in range(len(planes), rank))
+            planes[rank - 1].append((x, y))
+            x += 1
+            while x < y:
+                positions = tokenPositions[tokens[x]]
+                for xIndex in range(0, len(positions)):
+                    if positions[-1 - xIndex] == x:
+                        break
+                else:
+                    raise RuntimeError(
+                        f'position {x} missing from' \
+                            f' tokenPositions[{tokens[x]}]'
+                    )
+
+                usedYCount = usedYCounts[x]
+                if xIndex + 1 + usedYCount >= len(positions):
+                    x += 1
+                    continue
+
+                newY = positions[-1 - xIndex - 1 - usedYCount]
+                if newY < y:
+                    y = newY
+                    planes.extend([] for i in range(len(planes), rank))
+                    planes[rank - 1].append((x, y))
+                    usedYCounts[x] += 1
+
+                x += 1
+
+            rank += 1
+
+    path = []
+    x, y = 0, 0
+    for plane in reversed(planes):
+        firstBadIndex = bisect.bisect_right(plane, (x, float('inf')))
+        x, y = plane[firstBadIndex - 1]
+        path.append((x, y))
+
+    path.reverse()
+
+    result = np.zeros((len(tokens), len(tokens), 3), dtype=np.uint8)
+    for x, y in path:
+       result[y, x] = 255
+    # for i, plane in enumerate(planes):
+    #     hue = (i % 100) / 100
+    #     if hue < 1/6:
+    #         red = 1
+    #         green = 6 * hue
+    #         blue = 0
+    #     elif hue < 1/3:
+    #         red = 6 * (1/3 - hue)
+    #         green = 1
+    #         blue = 0
+    #     elif hue < 1/2:
+    #         red = 0
+    #         green = 1
+    #         blue = 6 * (hue - 1/3)
+    #     elif hue < 2/3:
+    #         red = 0
+    #         green = 6 * (2/3 - hue)
+    #         blue = 1
+    #     elif hue < 5/6:
+    #         red = 6 * (hue - 2/3)
+    #         green = 0
+    #         blue = 1
+    #     else:
+    #         red = 1
+    #         green = 0
+    #         blue = 6 * (1 - hue)
+
+    #     red, green, blue = (
+    #         int(np.clip(channel * 255, 0, 255)) \
+    #             for channel in (red, green, blue)
+    #     )
+
+    #     for x, y in plane:
+    #         result[y, x] = (red, green, blue)
 
     imwrite(
         imageFolder / (testCase + '.dtw.png'),
