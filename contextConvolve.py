84067f46cff80570850378bf3d1366931404456a
Eva Short <evashort73@gmail.com>
2020-05-07 21:00:07 -0700

    use common substrings as meta-tokens

--- contextConvolve.py
+++ contextConvolve.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import suffix_tree
 
 with open('./testcases/colors.json', 'rb') as f:
     text = f.read()
@@ -23,17 +24,6 @@ selectionStop = selectionStart + len(selectedText)
 
 tokens = np.frombuffer(text, dtype=np.byte)
 
-def getTokenWeights(tokens, decayRatio=0.6, minWeight=0.01):
-    weight = 1
-    tokenWeights = {}
-    for token in tokens:
-        tokenWeights[token] = tokenWeights.get(token, 0) + weight
-        weight *= decayRatio
-        if weight < minWeight:
-            break
-
-    return tokenWeights
-
 def smear(swatches, decayRatio=0.6, initialBlend=1):
     blend = initialBlend
     for swatch in swatches:
@@ -41,22 +31,42 @@ def smear(swatches, decayRatio=0.6, initialBlend=1):
         blend += swatch
         blend *= decayRatio
 
-beforeWeights = getTokenWeights(tokens[selectionStart - 1::-1])
-afterWeights = getTokenWeights(tokens[selectionStop:])
-beforeScores = np.fromiter(
-    smear(
-        beforeWeights.get(token, 0) for token in tokens
-    ),
-    dtype=float,
-    count=len(tokens)
-)
-afterScores = np.fromiter(
-    smear(
-        afterWeights.get(token, 0) for token in tokens[::-1]
-    ),
-    dtype=float,
-    count=len(tokens)
-)[::-1]
+def getPositions(children, textLength):
+    if children:
+        for child in children.values():
+            for stop in getPositions(child.children, textLength):
+                yield stop - len(child.text)
+    else:
+        yield textLength
+
+def getScores(tokens, start, decayRatio=0.6, minWeight=0.01):
+    scores = np.zeros(len(tokens))
+    weight = 1
+    root = suffix_tree.makeSuffixTree(tokens)
+    for pathStart in range(start, len(tokens)):
+        node = root
+        pathLength = 0
+        while node.children:
+            token = tokens[pathStart + pathLength]
+            node = node.children[token]
+            pathLength += len(node.text)
+
+            for position in getPositions(node.children, len(text)):
+                scores[position - pathLength] += weight * pathLength
+
+        weight *= decayRatio
+        if weight < minWeight:
+            break
+
+    scores = np.fromiter(
+        smear(scores[::-1], decayRatio=decayRatio),
+        dtype=float,
+        count=len(text)
+    )
+    return scores[::-1]
+
+beforeScores = getScores(tokens[::-1], len(tokens) - selectionStart)[::-1]
+afterScores = getScores(tokens, selectionStop)
 scores = beforeScores * afterScores
 plt.bar(nonSequenceIndices, scores[nonSequenceIndices])
 plt.bar(sequenceIndices, scores[sequenceIndices])
